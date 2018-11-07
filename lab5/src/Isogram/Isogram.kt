87b15a07781c483791dc572cdd3e0fc06ef9e62c
RobertsScetinins <36500775+RobertsScetinins@users.noreply.github.com>
2018-11-07 21:30:44 +0200

    Create Isogram.kt

---
+++ lab5/src/Isogram/Isogram.kt
@@ -0,0 +1,12 @@
+package Isogram
+
+object Isogram {
+    fun isIsogram(string: String): Boolean = !string
+                .toLowerCase()
+                .filter { !setOf(' ', '-').contains(it) }
+                .groupingBy { it }
+                .fold(0) { acc, value -> acc + 1 }
+                .any { it.value > 1 }
+
+
+}
